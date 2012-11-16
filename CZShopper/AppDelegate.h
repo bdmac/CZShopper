1735b1a4e7c181f410f062e50bd5dd3cc2dcc702
Brian McManus <bdmac97@gmail.com>
2012-11-16 16:17:55 -0500

    Initial Commit

---
+++ CZShopper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CZShopper
+//
+//  Created by Brian McManus on 11/16/12.
+//  Copyright (c) 2012 Brian McManus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CZShopper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CZShopper
+//
+//  Created by Brian McManus on 11/16/12.
+//  Copyright (c) 2012 Brian McManus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
